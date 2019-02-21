adea0624e1683895e60cb717137d053982c50381
Mohamed <mohamedsaadgadalhaq@gmail.com>
2019-02-21 14:32:47 +0200

    add project page

--- server.js
+++ server.js
@@ -42,7 +42,11 @@ app.get('/about', (req, res) => {
       pageTitle: 'About page',
      });
 });
-
+app.get('/projects', (req, res) => {
+ res.render('projects.hbs', {
+   pageTitle: 'Projects'
+ });
+});
 app.get('/bad', (req, res) => {
    res.send({
     errorMessage:'Unable to handle request'
